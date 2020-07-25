00849afa502e9712a32a85e0c72474ad4dd1d69b
LudwigVonChesterfield <andreydell@yahoo.com>
2020-07-25 20:43:38 +0300

    A better project representation on the PyPI site.

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
   name = 'tlingua',
   packages = ['tlingua'],
 
-  version = '000.000.000',
+  version = '000.000.001',
   license='MIT',
   description = 'A little library for text/grammar processing.',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
 
   author = 'Luduk',
   author_email = 'ningawent@gmail.com',
 
   url = 'https://github.com/LudwigVonChesterfield/TLingua',
-  download_url = 'https://github.com/LudwigVonChesterfield/TLingua/archive/v_000.000.000.tar.gz',
+  download_url = 'https://github.com/LudwigVonChesterfield/TLingua/archive/v_000.000.001.tar.gz',
 
   keywords = ['TAGS', 'STRINGS', 'GRAMMAR', 'LANGUAGE'],
 
